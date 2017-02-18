eb971459ad2c3744570da590283b2774d0c46399
Joseph Radford <joeradford@gmail.com>
2017-02-18 17:50:58 +1100

    Start key press handler, pseudo code for handling recording keys

--- flashgen/mainwindow.cpp
+++ flashgen/mainwindow.cpp
@@ -31,3 +31,34 @@ void MainWindow::setRecording(bool recording)
         // wait for first input
     }
 }
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_A:
+    case Qt::Key_S:
+    case Qt::Key_D:
+        if (!m_recording) {
+            // we are not acting on it, so pass it on
+            QMainWindow::keyPressEvent(event);
+        }
+        else {
+            if (event->type() == QEvent::KeyPress) {
+                // next event
+                // record pause since last event
+                // start recording hold for this event
+            }
+            else if (event->type() == QEvent::KeyRelease) {
+                // start the pause timer for this event
+                // record the hold timer for this event
+            }
+        }
+
+        break;
+
+    default:
+        // we are not acting on it, so pass it on
+        QMainWindow::keyPressEvent(event);
+        break;
+    }
+}
--- flashgen/mainwindow.h
+++ flashgen/mainwindow.h
@@ -19,6 +19,8 @@ public:
 
 private slots:
     void setRecording(bool recording);
+
+    void keyPressEvent(QKeyEvent *event);
 private:
     Ui::MainWindow *ui;
 
