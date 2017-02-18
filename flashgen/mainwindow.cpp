ffe5211739b352ca2b8926e9bca43e3b27fc6425
Joseph Radford <joeradford@gmail.com>
2017-02-18 18:21:02 +1100

    Key press and release events
    Call press and release in FlashRoutineObject
    Handle release in release event, not press event
    Check for auto repeat key events

--- flashgen/mainwindow.cpp
+++ flashgen/mainwindow.cpp
@@ -39,20 +39,15 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
     case Qt::Key_A:
     case Qt::Key_S:
     case Qt::Key_D:
-        if (!m_recording) {
-            // we are not acting on it, so pass it on
-            QMainWindow::keyPressEvent(event);
+        if (m_recording && !event->isAutoRepeat()) {
+            // next event
+            // record pause since last event
+            // start recording hold for this event
+            m_flashRoutine->startKeyPress(event->key());
         }
         else {
-            if (event->type() == QEvent::KeyPress) {
-                // next event
-                // record pause since last event
-                // start recording hold for this event
-            }
-            else if (event->type() == QEvent::KeyRelease) {
-                // start the pause timer for this event
-                // record the hold timer for this event
-            }
+            // we are not acting on it, so pass it on
+            QMainWindow::keyPressEvent(event);
         }
 
         break;
@@ -63,3 +58,29 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
         break;
     }
 }
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_A:
+    case Qt::Key_S:
+    case Qt::Key_D:
+        if (m_recording && !event->isAutoRepeat()) {
+            // start the pause timer for this event
+            // record the hold timer for this event
+            m_flashRoutine->stopKeyPress();
+        }
+        else {
+            // we are not acting on it, so pass it on
+            QMainWindow::keyReleaseEvent(event);
+        }
+
+        break;
+
+    default:
+        // we are not acting on it, so pass it on
+        QMainWindow::keyReleaseEvent(event);
+        break;
+    }
+}
+
